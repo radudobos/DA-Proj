687e7a10302fd0259a674776bccf36f95584d3c3
Radu Dobos <radudobos@Radus-MacBook-Pro.local>
2018-10-05 13:25:30 +0200

    Fix #1

--- templates/events.php
+++ templates/events.php
@@ -1,21 +1,36 @@
 <div class='intro events-intro'>
     <div class='container'>
         <h2>Where to find us ?</h2>
-        <div class='row'>
+        <div class='row event-cards '>
             <div class='col-sm-4'>
-                <h3>Company Day</h3>
-                <h4>05.09.2019</h4>
-                <p>Business Academy Aarhus</p>
+                <div class='col-sm-12'>
+                    <h3>Company Day</h3>
+                    <h4>05.09.2019</h4>
+                    <p>Business Academy Aarhus</p>
+                    <div class="cta">
+                                <a href="<?php home_url(); ?>/career/jobs/" class="btn btn-round">Read more</a>
+                    </div>
+                </div>
             </div>
             <div class='col-sm-4'>
-                <h3>Company Day</h3>
-                <h4>05.09.2019</h4>
-                <p>Business Academy Aarhus</p>
+                <div class='col-sm-12'>
+                        <h3>Company Day</h3>
+                        <h4>05.09.2019</h4>
+                        <p>Business Academy Aarhus</p>
+                        <div class="cta">
+                                    <a href="<?php home_url(); ?>/career/jobs/" class="btn btn-round">Read more</a>
+                        </div>
+                    </div>
             </div>
             <div class='col-sm-4'>
-                <h3>Company Day</h3>
-                <h4>05.09.2019</h4>
-                <p>Business Academy Aarhus</p>
+                <div class='col-sm-12'>
+                        <h3>Company Day</h3>
+                        <h4>05.09.2019</h4>
+                        <p>Business Academy Aarhus</p>
+                        <div class="cta">
+                                    <a href="<?php home_url(); ?>/career/jobs/" class="btn btn-round">Read more</a>
+                        </div>
+                    </div>
             </div>
 
         </div>
