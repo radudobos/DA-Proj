43b498b7c7a6183b7c441d61211fe6deb0d5ff0a
Radu Dobos <>
2018-10-09 12:58:32 +0200

    Turned section Dynamic

--- templates/departments.php
+++ templates/departments.php
@@ -1,6 +1,6 @@
 <div class="intro departments">
     <div class="container">
-        
+    <?php while ( have_posts() ) : the_post(); ?>
         <h2>Meet the different<br>Departments</h2>
         <h3 class ='text-center'>From day one, you will be assigned to a specific department within one of the companies matching your profile and motivations.</h3>
     <!-- Marketing -->
@@ -10,13 +10,9 @@
             </div>
             <div class='col-sm-6 text'>
                 <h3>Digital Marketing</h3>
-                <p>
-                    Being a part of the Digital Marketing Department will 
-                    give you hands-on experience within SEO, Google Analytics, 
-                    Marketing Automation, link-building strategies and a lot more.
-                </p>  
+                <p><?php echo get_post_meta($post->ID, 'digital_marketing', true); ?></p>  
                 <div class="cta">
-                    <a href="#cont" class="btn btn-default btn-round">Learn more</a>
+                    <a href="<?php home_url(); ?>/career/"  class="btn btn-default btn-round">Read more</a>
                 </div>
             </div>
         </div>
@@ -24,13 +20,9 @@
         <div class="row department">
             <div class='col-sm-6 text'>
                 <h3>UX & Design</h3>
-                <p>
-                    Working with Design & User Experience is all about 
-                    applying your knowledge in branding, design, usability, 
-                    and functions of our online platforms. 
-                </p>  
+                <p><?php echo get_post_meta($post->ID, 'ux', true); ?></p>  
                 <div class="cta">
-                    <a href="#cont" class="btn btn-default btn-round">Learn more</a>
+                    <a href="<?php home_url(); ?>/career/"  class="btn btn-default btn-round">Read more</a>
                 </div>
             </div>
             <div class='col-sm-6 pic '>
@@ -44,14 +36,9 @@
             </div>
             <div class='col-sm-6 text'>
                 <h3>Web Development</h3>
-                <p>
-                    When working in the Development department we will 
-                    teach you everything you need to know about layout and 
-                    visuals of a website, focusing on front-end development 
-                    tools such as Java, CSS etc.  
-                </p>  
+                <p><?php echo get_post_meta($post->ID, 'web', true); ?></p>  
                 <div class="cta">
-                    <a href="#cont" class="btn btn-default btn-round">Learn more</a>
+                    <a href="<?php home_url(); ?>/career/"  class="btn btn-default btn-round">Read more</a>
                 </div>
             </div>
         </div>
@@ -60,12 +47,9 @@
         <div class="row department">
             <div class='col-sm-6 text'>
                 <h3>Other</h3>
-                <p>
-                    We are always looking for different 
-                    interns with the right profile and mindset.   
-                </p>  
+                <p><?php echo get_post_meta($post->ID, 'other', true); ?></p>  
                 <div class="cta">
-                    <a href="#cont" class="btn btn-default btn-round">Learn more</a>
+                    <a href="<?php home_url(); ?>/career/"  class="btn btn-default btn-round">Read more</a>
                 </div>
             </div>
             <div class='col-sm-6 pic '>
@@ -73,6 +57,6 @@
             </div>
         </div>
 
-    
+    <?php endwhile; // end of the loop. ?>
     </div>
 </div>
--- templates/events.php
+++ templates/events.php
@@ -1,38 +1,41 @@
 <div class='intro events-intro' id="events-section">
     <div class='container'>
         <h2>Where to find us ?</h2>
+
+        <?php while ( have_posts() ) : the_post(); ?>
         <div class='row event-cards '>
             <div class='col-sm-4'>
                 <div class='col-sm-12'>
-                    <h3>Company Day</h3>
-                    <h4>05.09.2019</h4>
-                    <p>Business Academy Aarhus</p>
+                    <h3><?php echo get_post_meta($post->ID, '#1_event_name', true); ?></h3>
+                    <h4><?php echo get_post_meta($post->ID, '#1_event_date', true); ?></h4>
+                    <p><?php echo get_post_meta($post->ID, '#1_event_location', true); ?></p>
                     <div class="cta">
-                                <a href="<?php home_url(); ?>/career/jobs/" class="btn btn-round">Read more</a>
+                                <a href="<?php echo get_post_meta($post->ID, '#1_event_link', true); ?>" class="btn btn-round" target="_blank">Read more</a>
                     </div>
                 </div>
             </div>
             <div class='col-sm-4'>
                 <div class='col-sm-12'>
-                        <h3>Company Day</h3>
-                        <h4>05.09.2019</h4>
-                        <p>Business Academy Aarhus</p>
+                        <h3><?php echo get_post_meta($post->ID, '#2_event_name', true); ?></h3>
+                        <h4><?php echo get_post_meta($post->ID, '#2_event_date', true); ?></h4>
+                        <p><?php echo get_post_meta($post->ID, '#2_event_location', true); ?></p>
                         <div class="cta">
-                                    <a href="<?php home_url(); ?>/career/jobs/" class="btn btn-round">Read more</a>
+                                    <a href="<?php echo get_post_meta($post->ID, '#2_event_link', true); ?>" class="btn btn-round" target="_blank">Read more</a>
                         </div>
-                    </div>
+                </div>
             </div>
             <div class='col-sm-4'>
                 <div class='col-sm-12'>
-                        <h3>Company Day</h3>
-                        <h4>05.09.2019</h4>
-                        <p>Business Academy Aarhus</p>
+                        <h3><?php echo get_post_meta($post->ID, '#3_event_name', true); ?></h3>
+                        <h4><?php echo get_post_meta($post->ID, '#3_event_date', true); ?></h4>
+                        <p><?php echo get_post_meta($post->ID, '#3_event_location', true); ?></p>
                         <div class="cta">
-                                    <a href="<?php home_url(); ?>/career/jobs/" class="btn btn-round">Read more</a>
+                                    <a href="<?php echo get_post_meta($post->ID, '#3_event_link', true); ?>" class="btn btn-round" target="_blank">Read more</a>
                         </div>
                     </div>
             </div>
 
         </div>
+        <?php endwhile; // end of the loop. ?>
     </div>
 </div>
